76bbe3189421bf603d260a6fea266c2a360ecdf0
Coang <nguyenminhcoang@gmail.com>
2024-06-11 21:30:47 +0700

    Update order

--- src/main/java/com/example/api/OderController.java
+++ src/main/java/com/example/api/OderController.java
@@ -13,11 +13,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.dto.OrderDetailDTO;
 import com.example.entity.CategoryEntity;
 import com.example.entity.FoodEntity;
+import com.example.entity.Order;
 import com.example.entity.OrderDetail;
 import com.example.service.BillService;
 import com.example.service.CategoryService;
@@ -41,171 +44,169 @@ public class OderController {
 	@Autowired
 	private FoodService foodService;
 
-	List<BillItem> billItems = new ArrayList<>();
+	List<OrderDetailDTO> OrderDetailDTOs = new ArrayList<>();
 
 	@GetMapping("/{orderId}")
 	public String OrderView(Model model, @PathVariable Integer orderId) {
 		List<OrderDetail> orderDetails = orderDetailService.getOrderDetailsByOrder(orderId);
 		// model.addAttribute("name", "order");
-		billItems.clear();
+		OrderDetailDTOs.clear();
 
 		// Category
 		List<CategoryEntity> items = categoryService.getAllCategories();
 
 		// Bill
+		int i = 0;
 		for (OrderDetail item : orderDetails) {
-			billItems.add(
-					new BillItem(item.getFood().getFoodId(), item.getFood_number(), item.getFood().getFoodName(), item.getFood().getFoodPrice()));
-		}
-
-		Float total = 0f;
-		for (BillItem item : billItems) {
-			total += item.getPrice() * item.getQuantity();
+			OrderDetailDTOs.add(new OrderDetailDTO(i, item.getFood().getFoodId(), item.getFood_number(), item.getFood().getFoodName(),
+					item.getFood().getFoodPrice(), item.getOrder_note()));
+			i++;
 		}
 
 		List<FoodEntity> foods = foodService.getAllFoods();
 		model.addAttribute("orderId", orderId);
-		model.addAttribute("total", total);
-		model.addAttribute("billItems", billItems);
-		model.addAttribute("foods", foods);
-		model.addAttribute("categories", items);
-		return "order";
-	}
-	
-	@GetMapping("/{orderId}/")
-	public String OrderViewRl(Model model, @PathVariable Integer orderId) {
-		List<OrderDetail> orderDetails = orderDetailService.getOrderDetailsByOrder(orderId);
-		// model.addAttribute("name", "order");
-		billItems.clear();
-
-		// Category
-		List<CategoryEntity> items = categoryService.getAllCategories();
-
-		// Bill
-		for (OrderDetail item : orderDetails) {
-			billItems.add(
-					new BillItem(item.getFood().getFoodId(), item.getFood_number(), item.getFood().getFoodName(), item.getFood().getFoodPrice()));
-		}
-
-		Float total = 0f;
-		for (BillItem item : billItems) {
-			total += item.getPrice() * item.getQuantity();
-		}
-
-		List<FoodEntity> foods = foodService.getAllFoods();
-		model.addAttribute("orderId", orderId);
-		model.addAttribute("total", total);
-		model.addAttribute("billItems", billItems);
+		model.addAttribute("OrderDetailDTOs", OrderDetailDTOs);
 		model.addAttribute("foods", foods);
 		model.addAttribute("categories", items);
 		return "order";
 	}
 
-	@PostMapping("/{orderId}/addToBill/{foodId}")
-	public String addToBill(Model model, @PathVariable Integer foodId, @PathVariable Integer orderId) {
-		// Tìm FoodEntity tương ứng trong cơ sở dữ liệu dựa trên foodId
-		FoodEntity foodEntity = foodService.findFoodById(foodId);
+	@PostMapping("/{orderId}/save")
+	public ResponseEntity<String> saveOrder(@PathVariable Integer orderId, @RequestBody List<OrderDetailDTO> orders) {
+	    //Cập nhật danh sách order detail theo orderId
+		orderDetailService.deleteOrderDetailByOrder(orderId);
+	    for (OrderDetailDTO order : orders) {
+	        // Thực hiện cập nhật bill
+	        Integer foodId = order.getFoodId();
+	        Integer quantity = order.getQuantity();
+	        if(quantity > 0) {
+	        	FoodEntity foodEntity = foodService.findFoodById(foodId);
+	        	Order orderEntity = orderService.getOrderByOrder(orderId);
+	        	OrderDetail newOrderDetail = new OrderDetail();
+	        	newOrderDetail.setFood(foodEntity);
+				newOrderDetail.setOrder(orderEntity);
+				newOrderDetail.setFood_number(quantity);
+				Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+				newOrderDetail.setOrder_created_time(timestamp);
+				newOrderDetail.setOrder_modified_time(timestamp);
+				newOrderDetail.setOrder_note(order.getNote());
 
-		// Kiểm tra nếu foodEntity không tồn tại hoặc không hợp lệ
-		if (foodEntity == null) {
-			return "order";
-		}
-
-		// Tìm OrderDetail tương ứng trong cơ sở dữ liệu dựa trên orderId
-		List<OrderDetail> orderDetails = orderDetailService.getOrderDetailsByOrder(orderId);
-		
-		//Kiểm tra foodId đã tồn tại trong OrderDetail hay chưa
-		boolean exist = false;
-		for (OrderDetail item : orderDetails) {
-			//Nếu đã tồn tại thì tăng số lượng
-			if (item.getFood().getFoodId() == foodId) {
-				item.setFood_number(item.getFood_number() + 1);
-				Timestamp timestamp = new Timestamp(System.currentTimeMillis());	
-				item.setOrder_modified_time(timestamp);
-				orderDetailService.updateOrderDetail(item);
-				exist = true;
-				break;
-			}
-		}
-		
-		//Nếu chưa tồn tại thì tạo OrderDetail mới
-		if (!exist) {
-			// Tạo OrderDetail mới và truyền dữ liệu vào
-			OrderDetail orderDetail = new OrderDetail();
-			orderDetail.setFood(foodEntity);
-			orderDetail.setOrder(orderService.getOrderByOrder(orderId));
-			orderDetail.setFood_number(1);
-			Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-			orderDetail.setOrder_created_time(timestamp);
-			orderDetail.setOrder_modified_time(timestamp);
-
-			// Lưu OrderDetail vào cơ sở dữ liệu
-			orderDetailService.saveOrderDetail(orderDetail);
-		}
-		model.addAttribute("orderId", orderId);
-		return "order";
+				// Lưu OrderDetail vào cơ sở dữ liệu
+				orderDetailService.saveOrderDetail(newOrderDetail);
+	        }
+//	        // Tìm order detail và cập nhật
+//	        List<OrderDetail> orderDetails = orderDetailService.getOrderDetailsByOrder(orderId);
+//	        for (OrderDetail item : orderDetails) {
+//	        	boolean exist = false;
+//		        if (item.getFood().getFoodId() == foodId) {
+//			        item.setFood_number(quantity);
+//			        exist = true;
+//		        }
+//		        if(!exist) {
+//		        	FoodEntity foodEntity = foodService.findFoodById(foodId);
+//		        	Order orderEntity = orderService.getOrderByOrder(orderId);
+//		        	OrderDetail newOrderDetail = new OrderDetail();
+//		        	newOrderDetail.setFood(foodEntity);
+//					newOrderDetail.setOrder(orderEntity);
+//					newOrderDetail.setFood_number(1);
+//					Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+//					newOrderDetail.setOrder_created_time(timestamp);
+//					newOrderDetail.setOrder_modified_time(timestamp);
+//		
+//					// Lưu OrderDetail vào cơ sở dữ liệu
+//					orderDetailService.saveOrderDetail(newOrderDetail);
+//		        }
+//		        break;
+//	        }
+	    }
+	    
+//	    for(OrderDetail item : orderDetailService.getOrderDetailsByOrder(orderId)) {
+//	    	int i = 0;
+//		    for(OrderDetailDTO order : orders) {
+//			    if(item.getFood().getFoodId() == order.getFoodId()) {
+//				    i++;
+//			    }
+//		    }
+//		    if(i == 0) {
+//		    	orderDetailService.deleteOrderDetail(item);
+//		    }
+//	    }
+	    return ResponseEntity.ok("Order has been updated successfully");
 	}
-	
-	@PostMapping("/{orderId}/deleteFromBill")
-	public String deleteFromBill(Model model, @PathVariable Integer orderId) {
-		// Tìm OrderDetail tương ứng trong cơ sở dữ liệu dựa trên orderId
-		List<OrderDetail> orderDetails = orderDetailService.getOrderDetailsByOrder(orderId);
-		for (OrderDetail item : orderDetails) {
-			orderDetailService.deleteOrderDetail(item);
-		}
-		model.addAttribute("orderId", orderId);
-		return "order";
-	}
-	
-	@PostMapping("/{orderId}/updateQuantity/{foodId}/{quantity}")
-	public String updateQuantity(Model model, @PathVariable Integer foodId, @PathVariable Integer orderId, @PathVariable Integer quantity) {
-		// Tìm OrderDetail tương ứng trong cơ sở dữ liệu dựa trên orderId
-		List<OrderDetail> orderDetails = orderDetailService.getOrderDetailsByOrder(orderId);
-		for (OrderDetail item : orderDetails) {
-			if (item.getFood().getFoodId() == foodId) {
-				if(quantity > 0) {
-					item.setFood_number(quantity);
-					orderDetailService.updateOrderDetail(item);
-					break;
-				} else {
-					orderDetailService.deleteOrderDetail(item);
-					break;
-				}
-			}
-		}
-		return "order";
-	}
-	
-	
-	
-	public class BillItem {
-		private int quantity;
-		private String itemName;
-		private float price;
-		private Integer foodId;
 
-		public BillItem(Integer foodId, int quantity, String itemName, float price) {
-			this.foodId = foodId;
-			this.quantity = quantity;
-			this.itemName = itemName;
-			this.price = price;
-		}
+//	@PostMapping("/{orderId}/addToBill/{foodId}")
+//	public String addToBill(Model model, @PathVariable Integer foodId, @PathVariable Integer orderId) {
+//		// Tìm FoodEntity tương ứng trong cơ sở dữ liệu dựa trên foodId
+//		FoodEntity foodEntity = foodService.findFoodById(foodId);
+//
+//		// Kiểm tra nếu foodEntity không tồn tại hoặc không hợp lệ
+//		if (foodEntity == null) {
+//			return "order";
+//		}
+//
+//		// Tìm OrderDetail tương ứng trong cơ sở dữ liệu dựa trên orderId
+//		List<OrderDetail> orderDetails = orderDetailService.getOrderDetailsByOrder(orderId);
+//		
+//		//Kiểm tra foodId đã tồn tại trong OrderDetail hay chưa
+//		boolean exist = false;
+//		for (OrderDetail item : orderDetails) {
+//			//Nếu đã tồn tại thì tăng số lượng
+//			if (item.getFood().getFoodId() == foodId) {
+//				item.setFood_number(item.getFood_number() + 1);
+//				Timestamp timestamp = new Timestamp(System.currentTimeMillis());	
+//				item.setOrder_modified_time(timestamp);
+//				orderDetailService.updateOrderDetail(item);
+//				exist = true;
+//				break;
+//			}
+//		}
+//		
+//		//Nếu chưa tồn tại thì tạo OrderDetail mới
+//		if (!exist) {
+//			// Tạo OrderDetail mới và truyền dữ liệu vào
+//			OrderDetail orderDetail = new OrderDetail();
+//			orderDetail.setFood(foodEntity);
+//			orderDetail.setOrder(orderService.getOrderByOrder(orderId));
+//			orderDetail.setFood_number(1);
+//			Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+//			orderDetail.setOrder_created_time(timestamp);
+//			orderDetail.setOrder_modified_time(timestamp);
+//
+//			// Lưu OrderDetail vào cơ sở dữ liệu
+//			orderDetailService.saveOrderDetail(orderDetail);
+//		}
+//		model.addAttribute("orderId", orderId);
+//		return "order";
+//	}
 
-		public Integer getFoodId() {
-			return foodId;
-		}
-
-		public int getQuantity() {
-			return quantity;
-		}
-
-		public String getItemName() {
-			return itemName;
-		}
-
-		public float getPrice() {
-			return price;
-		}
-	}
+//	@PostMapping("/{orderId}/deleteFromBill")
+//	public String deleteFromBill(Model model, @PathVariable Integer orderId) {
+//		// Tìm OrderDetail tương ứng trong cơ sở dữ liệu dựa trên orderId
+//		List<OrderDetail> orderDetails = orderDetailService.getOrderDetailsByOrder(orderId);
+//		for (OrderDetail item : orderDetails) {
+//			orderDetailService.deleteOrderDetail(item);
+//		}
+//		model.addAttribute("orderId", orderId);
+//		return "order";
+//	}
+//	
+//	@PostMapping("/{orderId}/updateQuantity/{foodId}/{quantity}")
+//	public String updateQuantity(Model model, @PathVariable Integer foodId, @PathVariable Integer orderId, @PathVariable Integer quantity) {
+//		// Tìm OrderDetail tương ứng trong cơ sở dữ liệu dựa trên orderId
+//		List<OrderDetail> orderDetails = orderDetailService.getOrderDetailsByOrder(orderId);
+//		for (OrderDetail item : orderDetails) {
+//			if (item.getFood().getFoodId() == foodId) {
+//				if(quantity > 0) {
+//					item.setFood_number(quantity);
+//					orderDetailService.updateOrderDetail(item);
+//					break;
+//				} else {
+//					orderDetailService.deleteOrderDetail(item);
+//					break;
+//				}
+//			}
+//		}
+//		return "order";
+//	}
 
 }
---
+++ src/main/resources/static/js/order.js
@@ -0,0 +1,163 @@
+function adjustMainContentHeight() {
+	const topBarHeight = document.querySelector('.top-bar').offsetHeight;
+	const totalBarHeight = topBarHeight;
+	const topBillHeight = document.querySelector('.top-bill').offsetHeight;
+	const BotBillHeight = document.querySelector('.bot-bill').offsetHeight;
+	const totalBillHeight = topBillHeight + BotBillHeight;
+
+	const mainContent = document.getElementById('mainContent');
+	mainContent.style.height = `calc(100vh - ${totalBarHeight}px)`;
+
+	const mainContentBill = document.getElementById('bill');
+	mainContentBill.style.height = `calc(100vh - ${totalBarHeight}px - ${totalBillHeight}px - 50px)`;
+}
+
+// Gọi hàm khi tải trang
+window.onload = adjustMainContentHeight;
+// Gọi hàm khi thay đổi kích thước cửa sổ
+window.onresize = adjustMainContentHeight;
+
+//Thêm món ăn mới vào danh sách order, nếu đã tồn tại thì tăng số lượng
+function addToBill(button) {
+	const foodId = button.id;
+	const foodName = button.getAttribute('data-name');
+	const foodPrice = button.getAttribute('data-price');
+
+	orderItems.push({
+		id: i,
+		foodId: parseInt(foodId),
+		quantity: 1,
+		itemName: foodName,
+		price: parseFloat(foodPrice),
+		note: ''
+	});
+	i++;
+	length++;
+
+	updateBill();
+}
+
+
+//Cập nhật số lượng món ăn từ input
+function updateQuantity(id, inputQuantity) {
+	orderItems.forEach(item => {
+		if (item.id == id) {
+			item.quantity = parseInt(inputQuantity);
+		}
+	})
+	console.log(orderItems)
+	updateBill();
+}
+
+
+//Cập nhật danh sách order lên trang
+function updateBill() {
+	const billDiv = document.getElementById('bill');
+	billDiv.innerHTML = '';
+	orderItems.forEach(item => {
+		if (item.quantity > 0) {
+			const orderItemDiv = document.createElement('div');
+			orderItemDiv.classList.add('order-item', 'row', 'my-3');
+
+			const quantityInput = document.createElement('input');
+			quantityInput.type = 'number';
+			quantityInput.value = item.quantity;
+			quantityInput.min = '0';
+			quantityInput.id = item.id;
+			quantityInput.classList.add('quantity', 'col-sm-1', 'p-0', 'ms-3', 'fs-5');
+			quantityInput.onchange = () => updateQuantity(quantityInput.id, quantityInput.value);
+
+			const itemName = document.createElement('span');
+			itemName.classList.add('col-sm-6', 'ms-3', 'p-0', 'fs-5');
+			if (item.note != null && item.note != '') {
+				itemName.textContent = item.itemName + '(' + item.note + ')';
+			} else {
+				itemName.textContent = item.itemName;
+			}
+
+			itemName.addEventListener('click', function() {
+				const input = document.createElement('input');
+				input.type = 'text';
+				input.classList.add('col-sm-6', 'ms-3', 'p-0', 'fs-5');
+				input.value = item.note;
+				input.placeholder = 'note'
+
+				this.replaceWith(input);
+				input.focus();
+
+				input.addEventListener('blur', function() {
+					item.note = input.value;
+					if (item.note != null && item.note != '') {
+						itemName.textContent = item.itemName + '(' + item.note + ')';
+					} else {
+						itemName.textContent = item.itemName;
+					}
+					input.replaceWith(itemName);
+				});
+
+				input.addEventListener('keypress', function(e) {
+					if (e.key === 'Enter') {
+						item.note = input.value;
+						if (item.note != null && item.note != '') {
+							itemName.textContent = item.itemName + '(' + item.note + ')';
+						} else {
+							itemName.textContent = item.itemName;
+						}
+						input.replaceWith(itemName);
+					}
+				});
+			});
+			const priceDiv = document.createElement('div');
+			priceDiv.classList.add('col-sm-3', 'float-end', 'ms-3', 'p-0');
+			const itemPrice = document.createElement('span');
+			itemPrice.classList.add('p-0', 'fs-5');
+			itemPrice.textContent = `${item.price}vnđ`;
+			
+			priceDiv.appendChild(itemPrice)
+			orderItemDiv.appendChild(quantityInput);
+			orderItemDiv.appendChild(itemName);
+			orderItemDiv.appendChild(priceDiv);
+
+			billDiv.appendChild(orderItemDiv);
+		}
+		console.log(item.foodId + item.itemName + item.quantity + item.price)
+	})
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+	updateBill();
+});
+
+function deleteAllBill() {
+	const confirmed = confirm("Bạn có chắc chắn muốn xóa không?");
+	if (confirmed) {
+		orderItems.forEach(item => {
+			item.quantiy = 0;
+		})
+		
+		updateBill;
+	}
+}
+
+document.getElementById('back').onclick = function() {
+	console.log(orderItems)
+	const orderId = document.getElementById('bill').className;
+	// Gửi thông tin đơn hàng về server
+	fetch('/order/' + orderId + '/save', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(orderItems)
+	})
+		.then(response => response.json())
+		.then(data => {
+			console.log('Success:', data);
+			alert('Order has been completed successfully!');
+		})
+		.catch((error) => {
+			console.error('Error:', error);
+			alert('There was an error completing the order.');
+		});
+};
