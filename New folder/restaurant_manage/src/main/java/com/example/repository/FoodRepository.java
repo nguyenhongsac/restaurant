ddce26619dd900e77a3771e177d19fddf2a89f3e
taeitoilet <150056303+taeitoilet@users.noreply.github.com>
2024-06-21 23:34:02 +0700

    Delete New folder directory

--- New folder/restaurant_manage/src/main/java/com/example/repository/FoodRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.entity.FoodEntity;
-
-public interface FoodRepository extends JpaRepository<FoodEntity, Integer> {
-	
-}
--- New folder/src/main/java/com/example/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.entity.CategoryEntity;
-
-public interface CategoryRepository extends JpaRepository<CategoryEntity, Integer> {
-	
-}
